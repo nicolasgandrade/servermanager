09da4bd0fb0c7054a24f6ac681c0195b11d0d03c
nicolasgandrade <nicolasguerrero72@gmail.com>
2021-12-20 13:33:54 -0300

    config: populate the database for test

--- src/main/java/com/nicolasgandrade/servermanager/ServermanagerApplication.java
+++ src/main/java/com/nicolasgandrade/servermanager/ServermanagerApplication.java
@@ -1,7 +1,12 @@
 package com.nicolasgandrade.servermanager;
 
+import com.nicolasgandrade.servermanager.enums.Status;
+import com.nicolasgandrade.servermanager.model.Server;
+import com.nicolasgandrade.servermanager.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServermanagerApplication {
@@ -10,4 +15,35 @@ public class ServermanagerApplication {
 		SpringApplication.run(ServermanagerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return args -> {
+			serverRepository.save(new Server(
+					null,
+					"192.168.1.162",
+					"Fedora Linux",
+					"8GB",
+					"Personal computer",
+					"http://localhost:8080/servers/images/s1.png",
+					Status.SERVER_UP));
+
+			serverRepository.save(new Server(
+					null,
+					"192.164.2.156",
+					"AWS",
+					"8GB",
+					"Remote DB computer",
+					"http://localhost:8080/servers/images/s2.png",
+					Status.SERVER_UP));
+
+			serverRepository.save(new Server(
+					null,
+					"192.162.1.160",
+					"Ubuntu Linux",
+					"32GB",
+					"Remote Server",
+					"http://localhost:8080/servers/images/s3.png",
+					Status.SERVER_UP));
+		};
+	}
 }
